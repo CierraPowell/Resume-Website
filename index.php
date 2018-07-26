ddbb9c64b92385ce879fcae9bf087c7ce2249bad
Cierra <39931992+CierraPowell@users.noreply.github.com>
2018-07-26 13:25:55 -0500

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Cierra Powell Resume</title>
+<link rel="stylesheet" href="main.css"> <!--link to css-->
+
+</head>
+
+<body>
+
+    <?php include 'header.php';?>
+   
+
+ <div class="wrapper"> <!--pulling wrap styles-->
+
+    <div class="contentWrapper">
+
+        <div id="photo">
+            <img src="cierrapowell.jpg" width="200px">
+        </div>  
+
+        <div class="navigation">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+        </div>
+
+
+        <div class="mainContent">
+         
+        <h1>Experience</h1>
+          
+          
+          
+          <h2>Back Bay Seafood Restaurant</h2>
+          <p>Hostess<br>
+            Gulfport, Ms <em>03/2011 - 10/2012</em></p>
+          <p>Greet guest, properly seat guest according to 
+              server seating chart, recorded to-go orders and 
+              accurately entered into a POS system, set schedule 
+              party tables up prior to arrival, buss/reset tables, 
+              use of POS system, etc.</p>
+
+            <h2>The Hook Up Restaurant</h2>
+          <p>Hostess/Expo<br>
+            Biloxi, Ms <em>10/2012 - 07/2014</em></p>
+          <p>Greet guest, properly seat guest according to server 
+              seating chart, recorded to-go orders and accurately
+               entered into a POS system, set schedule party 
+               tables up prior to arrival, buss/reset tables, 
+               use of POS system, expedited food, kitchen prep, 
+               trayed/carried food to tables on large trays using 
+               service legs, etc.</p>
+
+            <h2>Cora's Restaurant</h2>
+          <p>Hostess<br>
+            Biloxi, Ms <em>07/2014 - 02/2016</em></p>
+          <p>Greet guest, properly seat guest according 
+              to server seating chart, recorded to-go orders 
+              and accurately entered into a POS system, set schedule 
+              party tables up prior to arrival, buss/reset tables, use 
+              of POS system, Set up/ break down catering events, etc.</p>
+        
+        <h2>The Biloxi Lugger Restaurant</h2>
+          <p>Waitress/Floor Manager<br>
+            Biloxi, Ms <em>09/2015 - 08/2016</em></p>
+            <p> Met with Chef to learn specials of the day and
+                 items out of stock, uploaded images to social media 
+                 page for proper advertising on specials and upcoming events, 
+                 designed a daily/weekly section rotational sheet for waitresses, 
+                 hire/terminations, implemented training and retraining when necessary, 
+                ordered office/bar/front of house items, scheduling of employees 
+                and live music, cash out waiters at end of shift and ran reports, 
+                deposits and kept small denominations in petty cash, personally 
+                worked any position in house if shorthanded, scheduled maintenance, etc.</p>
+
+        <h2>Baja Beach Biloxi Restaurant</h2>
+          <p>Waitress<br>
+            Biloxi, Ms <em>04/2017 - 03/2018</em></p>
+            <p> 
+            Greet guest, properly seat guest according to server seating
+            chart, recorded to-go orders and accurately entered into a POS
+            system, buss/reset tables, use of POS system, accurately inform
+            guests of any/ all specials, be knowledgeable of menu for allergy
+            related issues, etc.
+            </p>
+
+        <h2>Bacchus Biloxi Restaurant</h2>
+          <p>Waitress<br>
+            Biloxi, Ms <em>04/2018 - Present</em></p>
+            <p>Greet guest, properly seat guest according to server seating
+            chart, recorded to-go orders and accurately entered into a POS
+            system, buss/reset tables, use of POS system, accurately inform
+            guests of any/ all specials, be knowledgeable of menu for allergy
+            related issues, etc.</p>
+
+        <h1>Education</h1>
+          
+          <h2>Mississippi Gulf Coast Community College</h2>
+          <p>Hotel and Restaurant Management, <em>2011 - 2015</em></p>
+        </div>  
+
+    </div>
+
+ </div>
+ 
+ <?php include 'footer.php';?> 
+
+</body>
+</html>
