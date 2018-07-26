bc9362f6ff893a3facf1cb801878213bc99d9e7e
Cierra <39931992+CierraPowell@users.noreply.github.com>
2018-07-26 13:27:42 -0500

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,16 @@
+<div id="formDiv">
+          <h1>Contact Me</h1>
+          <form>
+           
+            Name<br> 
+            <input type="text" name="name" class="inputField" required><br>
+            
+            Email Address<br>
+            <input type="email" name="email" class="inputField" required><br>
+            
+            Message<br>
+            <input type="textarea" name="message" class="textAreaInput" required><br>
+            
+            <input type="submit" class="submitButton">
+          </form>
+        </div>
